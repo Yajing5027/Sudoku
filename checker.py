3ccd8460eea08e64e9caa24c4636bbaef3de8791
Hoda Omar <np9771ah@go.minnstate.edu>
2025-11-12 15:24:15 -0600

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,6 +1,6 @@
 def is_move_valid(self,row,col,num):
   #checks the row
-  if number in self.board[row]:
+  if num in self.board[row]:
     return False
   #checks the col
   for r in range(9):
@@ -10,7 +10,12 @@ def is_move_valid(self,row,col,num):
   box_row = (row // 3) * 3
   box_col = (col // 3) * 3
   for r in range(box_row, box_row +3):
-    for c in range(box_col, bow_col +3):
+    for c in range(box_col, box_col +3):
       if self.board[r][c] == num:
         return False
-      
+  return True
+
+def is_solved(board):
+  if row in board:
+    
+    
