9471d457cb379388f0668250ddba0aeb376f45f2
Ese-teni <xb3415mj@go.minnstate.edu>
2025-11-12 15:23:25 -0600

    Update display.py

---
+++ display.py
@@ -0,0 +1,13 @@
+board=[[1,2,3,4],
+       [4,3,2,1],
+       [3,1,4,2],
+       [2,4,1,3]]
+print(board[1][3])
+'''def display_board(board):
+ print("   C1 C2 C3 C4")
+ for i in range(4):
+   print(f'R{i+1}',end="")
+   print()
+display_board(board)'''
+
+
