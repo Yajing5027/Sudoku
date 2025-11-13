df7361dd0e4bb1d4a50d32b4ac194aff9a09f6b9
Yajing <2946547864@qq.com>
2025-11-12 18:16:28 -0600

    update generator.py achieve complete_board method

--- generator.py
+++ generator.py
@@ -1,25 +1,36 @@
 import random
 
-class board:
+class Board:
     def __init__(self,size):
-        self.size = size
-        self.base = size**0.5
-        num = []
+        self.size = size        # outer box size (4 or 9)
+        self.base = int(size**0.5)       # inner box size = root of outer one (2 or 3), but will output floar, must add int for later % operate
+        self.board = []     # save final 2D list
         
     def complete_board(self):
-        col_list =[i + 1 for i in range(self.size)]
-        row_list =[i + 1 for i in range(self.size)]
-        for i in range(self.size):
-            for j in range(self.size):
-                (i * base + i // base + j) % size
+        size = self.size
+        base = self.base
 
+        nums = [i for i in range(1,size+1)]        # set sudoku available numbers range
 
-        def shuffle_board(self):
-            random.shuffle()
+        # make 2D list
+        row_list =[]        # involve all row
+        for row in range(size):
+            each_row = []       # involve all col of one row
+            for col in range(size):
+                value = ((row * base + row // base + col) % size) + 1
+                each_row.append(value)
+            row_list.append(each_row)
+        
+        self.board = row_list       # renew board
+        return self.board
 
-    def blank_board(self):
+'''        def shuffle_board(self):
+        random.shuffle()
+
+    def blank_board(self):'''
         
 
-board1 = board(9)
+board1 = Board(9)
+print(board1.complete_board())
 
 
