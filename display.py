c766e4b136825aa5949ab14dc1dc0836bb98f73f
Ese-teni <xb3415mj@go.minnstate.edu>
2025-11-12 21:06:22 -0600

    Update display.py
    12

--- display.py
+++ display.py
@@ -1,13 +1,25 @@
-board=[[1,2,3,4],
-       [4,3,2,1],
-       [3,1,4,2],
-       [2,4,1,3]]
-print(board[1][3])
-'''def display_board(board):
- print("   C1 C2 C3 C4")
- for i in range(4):
-   print(f'R{i+1}',end="")
-   print()
-display_board(board)'''
+class Sudoku:
+ def __init__(self):
+   self.board=self.generate_board()
+ def generate_board(self):
+    board = [
+        [1, 0, 3, 4],
+        [0, 3, 0, 1],
+        [3, 4, 1, 0],
+        [0, 1, 0, 2]
+    ]
+    return board
+ def display_board(self):
+  print("   C1 C2 C3 C4")
+  for i in range(4):
+    print(f'R{i+1} ',end="")
+    for j in range(4):
+      value=self.board[i][j]
+      if value == 0:
+         print("_", end="  ")  # show underscore for blanks
+      else:
+         print(value, end="  ")
 
-
+    print()  # move to next li
+soduku=Sudoku()
+soduku.display_board()
