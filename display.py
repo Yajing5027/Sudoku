f1f741d44cd276c64887f56b525797a76fb6c525
Ese-teni <xb3415mj@go.minnstate.edu>
2025-11-12 21:31:54 -0600

    Update display.py

--- display.py
+++ display.py
@@ -3,17 +3,24 @@ class Sudoku:
    self.board=self.generate_board()
  def generate_board(self):
     board = [
-        [1, 0, 3, 4],
-        [0, 3, 0, 1],
-        [3, 4, 1, 0],
-        [0, 1, 0, 2]
-    ]
+    [0, 2, 7, 1, 0, 4, 3, 9, 0],
+    [0, 6, 5, 3, 2, 0, 1, 0, 8],
+    [3, 4, 1, 0, 0, 8, 7, 0, 2],
+    [5, 0, 3, 4, 0, 1, 0, 2, 7],
+    [4, 7, 2, 8, 3, 5, 9, 6, 1],
+    [6, 0, 8, 9, 7, 0, 4, 3, 5],
+    [7, 8, 0, 0, 1, 9, 5, 0, 4],
+    [1, 5, 0, 7, 8, 6, 2, 0, 9],
+    [2, 0, 9, 5, 4, 0, 6, 7, 0]
+]
+        
+
     return board
  def display_board(self):
-  print("   C1 C2 C3 C4")
-  for i in range(4):
+  print("   C1 C2 C3 C4 C5 C6 C7 C8 C9")
+  for i in range(9):
     print(f'R{i+1} ',end="")
-    for j in range(4):
+    for j in range(9):
       value=self.board[i][j]
       if value == 0:
          print("_", end="  ")  # show underscore for blanks
